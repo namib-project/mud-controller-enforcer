4256749f6e2e7203bb9e68ff3e039bbcf83fcce4
Michael Bach <bachm@uni-bremen.de>
2020-11-13 03:01:02 +0100

    Fixed cert scripts

--- certs/gen_client_cert.sh
+++ certs/gen_client_cert.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-openssl genrsa -out ../../namib_enforcer/client-key.pem 4096
-openssl req -new -key ../namib_enforcer/client-key.pem -config client.cnf -out client.csr
-openssl x509 -req -in client.csr -days 3650 -CA ca.pem -CAkey ca-key.pem -CAcreateserial -extfile client.cnf -extensions usr_cert -out ../namib_enforcer/client.pem
+openssl genrsa -out ../../namib_enforcer/certs/client-key.pem 4096
+openssl req -new -key ../../namib_enforcer/certs/client-key.pem -config client.cnf -out client.csr
+openssl x509 -req -in client.csr -days 3650 -CA ca.pem -CAkey ca-key.pem -CAcreateserial -extfile client.cnf -extensions usr_cert -out ../../namib_enforcer/certs/client.pem
--- certs/gen_server_cert.sh
+++ certs/gen_server_cert.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-openssl genrsa -out ../namib_mud_controller/server-key.pem 4096
-openssl req -new -key ../namib_mud_controller/server-key.pem -config server.cnf -out server.csr
-openssl x509 -req -in server.csr -days 3650 -CA ca.pem -CAkey ca-key.pem -CAcreateserial -extfile server.cnf -extensions server_cert -out ../namib_mud_controller/server.pem
+openssl genrsa -out ../../namib_mud_controller/certs/server-key.pem 4096
+openssl req -new -key ../../namib_mud_controller/certs/server-key.pem -config server.cnf -out server.csr
+openssl x509 -req -in server.csr -days 3650 -CA ca.pem -CAkey ca-key.pem -CAcreateserial -extfile server.cnf -extensions server_cert -out ../../namib_mud_controller/certs/server.pem
