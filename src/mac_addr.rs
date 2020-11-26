2dedd47328c94ad6ec9c4e3dc148487c6a4f3b62
Benjamin Ludewig <benjamin.ludewig@neuland-bfi.de>
2020-11-26 23:55:18 +0100

    add devices_controller

--- src/mac_addr.rs
+++ src/mac_addr.rs
@@ -3,12 +3,36 @@ use serde::{Deserialize, Serialize};
 
 use core::fmt;
 pub use macaddr;
+use schemars::{
+    gen::SchemaGenerator,
+    schema::{InstanceType, Schema, SchemaObject},
+    JsonSchema,
+};
 
 /// A MAC address, either in *EUI-48* or *EUI-64* format.
 #[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Serialize, Deserialize)]
 pub enum MacAddr {
     V6(MacAddr6),
     V8(MacAddr8),
+}
+
+impl JsonSchema for MacAddr {
+    fn is_referenceable() -> bool {
+        false
+    }
+
+    fn schema_name() -> String {
+        "MacAddr".to_string()
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            format: Some("mac".to_string()),
+            ..Default::default()
+        }
+        .into()
+    }
 }
 
 impl From<macaddr::MacAddr> for MacAddr {
