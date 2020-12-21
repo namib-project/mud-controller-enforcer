2b27eca46b8e8cb24f1aaef9870585133f9a54b8
Michael Bach <bachm@uni-bremen.de>
2020-12-21 15:42:34 +0100

    Changed SQLite to use In-Memory DB

--- tests/lib.rs
+++ tests/lib.rs
@@ -19,10 +19,10 @@ impl IntegrationTestContext {
         dotenv().ok();
         env_logger::try_init().ok();
 
-        let base_url = env::var("TESTING_DATABASE_URL").expect("Failed to load DB URL from .env");
+        let base_url = env::var("TESTING_POSTGRES_URL").expect("Failed to load DB URL from .env");
 
         #[cfg(feature = "sqlite")]
-        let db_url = format!("{}test-{}.db", ensure_trailing_slash(base_url), db_name);
+        let db_url = ":memory:".to_string();
 
         #[cfg(feature = "postgres")]
         let db_url = format!("{}/{}", base_url, db_name);
@@ -39,7 +39,7 @@ impl IntegrationTestContext {
         );
 
         // pool::get_one().expect("Couldn't establish connection to database")
-        let migration_conn = ConnectionType::establish(db_url.as_ref()).expect("Couldn't establish connection to database");
+        let migration_conn = pool.get_one().expect("Couldn't establish connection to database");
         run_db_migrations(&migration_conn).expect("Database migrations failed");
         drop(migration_conn);
 
@@ -61,27 +61,6 @@ impl Drop for IntegrationTestContext {
         // Closing all connections by destructing db_pool implicitly, so we can delete the db
         self.db_pool = None;
 
-        #[cfg(feature = "sqlite")]
-        remove_sqlite_db(self.borrow());
         info!("Cleaned up database context");
     }
-}
-
-/// Ensures that given string has a trailing slash, if not: returns the string with a trailing slash
-fn ensure_trailing_slash(input: String) -> String {
-    let last_char = match input.chars().last() {
-        Some(char) => char,
-        None => return "".to_string(),
-    };
-
-    match last_char == '/' {
-        true => input,
-        false => input + "/",
-    }
-}
-
-/// Basic wrapper for deleting a SQLite Database at given IntegrationTestContext db_url path
-fn remove_sqlite_db(context: &IntegrationTestContext) -> () {
-    debug!("Removing SQLite DB at {}", context.db_url);
-    fs::remove_file(context.db_url.clone()).expect(format!("Couldn't remove SQLite DB at {}", context.db_url).as_ref())
 }
