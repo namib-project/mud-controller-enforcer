1952244ae49edc77896f61287264637f23974629
Jeffrey <jemu@uni-bremen.de>
2021-02-14 12:52:34 +0100

    Using ON CONFLICT clause for user config upsert.

--- src/services/user_config_service.rs
+++ src/services/user_config_service.rs
@@ -21,51 +21,17 @@ pub async fn get_config_for_user(user_id: i64, key: &str, conn: &DbConnection) -
     .await?)
 }
 
-async fn update_config_for_user(user_id: i64, key: &str, value: &str, conn: &DbConnection) -> error::Result<u64> {
-    let upd_count = sqlx::query!(
-        "update user_configs set value = ? where key = ? and user_id = ?",
+pub async fn upsert_config_for_user(user_id: i64, key: &str, value: &str, conn: &DbConnection) -> error::Result<()> {
+    let _ = sqlx::query!(
+        "INSERT INTO user_configs VALUES (?, ?, ?) ON CONFLICT(user_id, key) DO UPDATE SET value = excluded.value",
+        key,
+        user_id,
         value,
-        key,
-        user_id
     )
     .execute(conn)
     .await?;
 
-    debug!("Updating key {:?} with value {:?} for user {:?}", key, value, user_id);
-
-    Ok(upd_count.rows_affected())
-}
-
-async fn add_config_for_user(user_id: i64, key: &str, value: &str, conn: &DbConnection) -> error::Result<u64> {
-    let ins_count = sqlx::query!(
-        "insert into user_configs (key, user_id, value) values (?, ?, ?)",
-        key,
-        user_id,
-        value
-    )
-    .execute(conn)
-    .await?;
-
-    debug!("Inserting key {:?} with value {:?} for user {:?}", key, value, user_id);
-
-    Ok(ins_count.rows_affected())
-}
-
-pub async fn upsert_config_for_user(user_id: i64, key: &str, value: &str, conn: &DbConnection) -> error::Result<u64> {
-    let existing_config: Vec<UserConfig> = sqlx::query_as!(
-        UserConfig,
-        "select * from user_configs where user_id = ? and key = ?",
-        user_id,
-        key
-    )
-    .fetch_all(conn)
-    .await?;
-
-    return if existing_config.is_empty() {
-        Ok(add_config_for_user(user_id, key, value, conn).await?)
-    } else {
-        Ok(update_config_for_user(user_id, key, value, conn).await?)
-    };
+    Ok(())
 }
 
 pub async fn delete_config_for_user(user_id: i64, key: &str, conn: &DbConnection) -> error::Result<u64> {
