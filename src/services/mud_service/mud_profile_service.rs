66dde8d649b5188a1b4ef57e9fed36bf0017b003
Luca Nittscher <lnittsch@uni-bremen.de>
2021-02-26 12:56:54 +0100

    test_update_outdated_profiles() doku

--- src/services/mud_service/mud_profile_service.rs
+++ src/services/mud_service/mud_profile_service.rs
@@ -135,10 +135,14 @@ mod tests {
 
         Ok(db_conn)
     }
+
     #[actix_rt::test]
+    //tests whether update_outdated_profiles() works on expired profiles. Dependent on external Service.
     async fn test_update_outdated_profiles() -> Result<()> {
+        //Sets up an expired Amazon Echo profile
         const PATH: &str = "tests/mud_tests/Amazon-Echo";
         let conn = init().await?;
+          //external URL containing the same contents as in the test file. Makes the test dependent on an external Service
         let url: String = String::from("http://iotanalytics.unsw.edu.au/mud/amazonEchoMud.json");
         let mut file = File::open(PATH).expect(format!("Could not open {}", PATH).as_str());
         let mut str_data = String::new();
@@ -154,9 +158,11 @@ mod tests {
             url: url.to_owned(),
             data: serde_json::to_string(&mud_data)?,
             created_at: Utc::now().naive_utc(),
+            //the expiration time is set to an arbitrary value that is guaranteed to be prior to the current date
             expiration: (present - Duration::hours(duration)).naive_utc(),
         };
 
+        //Puts expired Profile into the Database
         sqlx::query!(
             "insert into mud_data (url, data, created_at, expiration) values (?, ?, ?, ?)",
             mud_dbo.url,
@@ -167,20 +173,28 @@ mod tests {
         .execute(&conn)
         .await?;
 
+        //function call
         update_outdated_profiles(&conn).await?;
 
         let new_mud_data: MudDbo = sqlx::query_as!(MudDbo, "SELECT * FROM mud_data WHERE url = $1", mud_data.url)
             .fetch_one(&conn)
             .await?;
 
+        //value slightly below and above the time that the new expiration date should be active for
         let lower_expiration = present.naive_utc() + Duration::hours(duration - 1);
         let higher_expiration = present.naive_utc() + Duration::hours(duration + 1);
 
+        //checks whether the expiration date has been changed
         assert_ne!(new_mud_data.expiration, mud_dbo.expiration);
+        //checks whether the new expiration date is more recent than the previous one
         assert!(new_mud_data.expiration > mud_dbo.expiration);
+        //checks whether the new expiration date is after the current date
+        assert!(new_mud_data.expiration > present.naive_utc());
+        //checks whether the new expiration date is within the bounds of the cache validity
         assert!(new_mud_data.expiration > lower_expiration);
         assert!(new_mud_data.expiration < higher_expiration);
 
+        //returns the database to the state before the test
         sqlx::query!("DELETE FROM mud_data WHERE url = ?", mud_dbo.url)
             .execute(&conn)
             .await?;
@@ -189,13 +203,18 @@ mod tests {
             .fetch_optional(&conn)
             .await?;
 
+        //makes sure the tests changes to the database are gone
         assert!(is_delete.is_none());
         Ok(())
     }
+
     #[actix_rt::test]
+    //tests whether update_outdated_profiles() doesn't modify profiles that aren't expired yet. Dependent on external Service.
     async fn test_update_valid_profiles() -> Result<()> {
+        //Sets up a valid Amazon Echo profile
         const PATH: &str = "tests/mud_tests/Amazon-Echo";
         let conn = init().await?;
+        //external URL containing the same contents as in the test file. Makes the test dependent on an external Service
         let url: String = String::from("http://iotanalytics.unsw.edu.au/mud/amazonEchoMud.json");
         let duration: i64 = 50;
         let mut file = File::open(PATH).expect(format!("Could not open {}", PATH).as_str());
@@ -208,11 +227,14 @@ mod tests {
 
         let mud_dbo = MudDbo {
             url: url.to_owned(),
+            //the profile content is modified from it's original state to distinguish it from the original
             data: serde_json::to_string(&mud_data)? + "Test",
             created_at: Utc::now().naive_utc(),
+            //the expiration time is set to an arbitrary value that is after the current date
             expiration: (Utc::now() + Duration::hours(duration)).naive_utc(),
         };
 
+        //Puts active Profile into the Database
         sqlx::query!(
             "insert into mud_data (url, data, created_at, expiration) values (?, ?, ?, ?)",
             mud_dbo.url,
@@ -223,19 +245,24 @@ mod tests {
         .execute(&conn)
         .await?;
 
+        //function call
         update_outdated_profiles(&conn).await?;
 
         let new_mud_data: MudDbo = sqlx::query_as!(MudDbo, "SELECT * FROM mud_data WHERE url = $1", mud_data.url)
             .fetch_one(&conn)
             .await?;
 
+        //checks whether the expiration date has not been changed
         assert_eq!(new_mud_data.expiration, mud_dbo.expiration);
+        //checks that the contents of the profile have not been updated
         assert_eq!(
             serde_json::to_string(&new_mud_data).unwrap(),
             serde_json::to_string(&mud_dbo).unwrap()
         );
+        //checks whether the expiration date is after the current date
         assert!(new_mud_data.expiration > Utc::now().naive_utc());
 
+        //returns the database to the state before the test
         sqlx::query!("DELETE FROM mud_data WHERE url = ?", mud_dbo.url)
             .execute(&conn)
             .await?;
@@ -244,6 +271,7 @@ mod tests {
             .fetch_optional(&conn)
             .await?;
 
+        //makes sure the tests changes to the database are gone
         assert!(is_delete.is_none());
         Ok(())
     }
