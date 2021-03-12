5529ed67ac1007d743d02f049e94a2181082aa71
Benjamin Ludewig <ludewig.b@gmail.com>
2021-03-12 19:59:16 +0100

    add OpenAPI descriptions

--- src/routes/status_controller.rs
+++ src/routes/status_controller.rs
@@ -5,12 +5,12 @@ pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.route("", web::get().to(get_status));
 }
 
-#[api_v2_operation]
+#[api_v2_operation(summary = "Retrieve the version and setup status of this controller")]
 async fn get_status(pool: web::Data<DbConnection>) -> Result<Json<StatusDto>> {
     let has_any_users = user_service::has_any_users(pool.get_ref()).await?;
 
     Ok(Json(StatusDto {
-        setup_required: has_any_users,
+        setup_required: !has_any_users,
         version: VERSION,
     }))
 }
