2f8bdb5df694fa5cec59ba150993737b65b13b2f
Luca Nittscher <lnittsch@uni-bremen.de>
2021-03-19 20:29:40 +0100

    Added all room_service functions

--- src/services/room_service.rs
+++ src/services/room_service.rs
@@ -22,6 +22,14 @@ pub async fn find_by_id(id: i64, pool: &DbConnection) -> Result<Room> {
     Ok(room)
 }
 
+pub async fn find_by_name(name: String, pool: &DbConnection) -> Result<Room> {
+    let room = sqlx::query_as!(Room, "select * from rooms where name = ?", name)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(room)
+}
+
 /*pub fn find_by_optional_id(id: Option<i64>, pool: &DbConnection) -> Result<Option<Room>> {
     match id {
         Some(room) => Ok(Some(find_by_id(room, pool))),
@@ -33,4 +41,55 @@ pub fn convert_devicedbo_and_room_to_device(device: DeviceDbo, room: Room) -> De
     let mut device_with_room = Device::from(device);
     device_with_room.room = Some(room);
     device_with_room
+}
+
+pub async fn update(id: i64, room: &Room, conn: &DbConnection) -> Result<u64> {
+    let upd_count = sqlx::query!(
+        "update rooms set name = ?, color = ? where room_id = ?",
+        room.name,
+        room.color,
+        id
+    )
+        .execute(conn)
+        .await?;
+
+    Ok(upd_count.rows_affected())
+}
+
+pub async fn get_all_rooms(pool: &DbConnection) -> Result<Vec<Room>> {
+    let room_data = sqlx::query_as!(Room, "select * from rooms").fetch_all(pool).await?;
+
+    Ok(room_data)
+}
+
+pub async fn get_all_devices_in_room(id: i64, pool: &DbConnection) -> Result<Vec<Device>> {
+    let devices = sqlx::query_as!(DeviceDbo, "select * from devices where room_id = ?", id).fetch(pool);
+
+    let devices_data = devices
+            .err_into::<Error>()
+            .and_then(|device| async {
+                let mut device_data = Device::from(device);
+                device_data.mud_data = match device_data.mud_url.clone() {
+                    Some(url) => {
+                        let data = get_mud_from_url(url.clone(), pool).await;
+                        debug!("Get all devices: mud url {:?}: {:?}", url, data);
+                        data.ok()
+                    },
+                    None => None,
+                };
+
+                Ok(device_data)
+            })
+            .try_collect::<Vec<Device>>()
+            .await?;
+
+        Ok(devices_data)
+    }
+
+pub async fn delete_room(id: i64, pool: &DbConnection) -> Result<u64> {
+    let del_count = sqlx::query!("delete from rooms where room_id = ?", id)
+        .execute(pool)
+        .await?;
+
+    Ok(del_count.rows_affected())
 }
