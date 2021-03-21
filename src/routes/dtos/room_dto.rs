bc6c1ed358fbce489565d9f48ae0d4560cf09dcc
lax <flobon@uni-bremen.de>
2021-03-21 15:02:07 +0100

    Created created room_controller.rs, room_dto.rs

---
+++ src/routes/dtos/room_dto.rs
@@ -0,0 +1,21 @@
+#![allow(clippy::field_reassign_with_default)]
+
+use crate::models::{Device, MudData, Room};
+use paperclip::actix::Apiv2Schema;
+
+#[derive(Debug, Serialize, Deserialize, Apiv2Schema)]
+pub struct RoomDto {
+    pub id: i64,
+    pub name: String,
+    pub color: String,
+}
+
+impl From<Room> for RoomDto {
+    fn from(room: Room) -> Self {
+        RoomDto {
+            id: r.room_id,
+            name: room.name,
+            color: room.color,
+        }
+    }
+}
---
+++ src/routes/room_controller.rs
@@ -0,0 +1,17 @@
+#![allow(clippy::needless_pass_by_value)]
+
+use paperclip::actix::{api_v2_operation, web, web::Json};
+
+use crate::{auth::AuthToken, db::DbConnection, error::Result, routes::dtos::DeviceDto, services::room_service};
+
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.route("", web::get().to(get_all_rooms));
+}
+
+#[api_v2_operation]
+async fn get_all_rooms(pool: web::Data<DbConnection>, auth: AuthToken) -> Result<Json<Vec<DeviceDto>>> {
+    auth.require_permission("room/list")?;
+    let res = room_service::get_all_rooms(pool.get_ref()).await?;
+    info!("{:?}", res);
+    Ok(Json(res.into_iter().map(RoomDto::from).collect()))
+}
