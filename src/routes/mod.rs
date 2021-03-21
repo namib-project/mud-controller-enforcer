03eef1d130b0fcfbc2d54ae59909ef2222fecc38
lax <flobon@uni-bremen.de>
2021-03-21 22:07:19 +0100

    Formatting

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,6 +3,6 @@ pub mod device_controller;
 pub mod dtos;
 pub mod mud_controller;
 pub mod role_manager_controller;
+pub mod room_controller;
 pub mod status_controller;
 pub mod users_controller;
-pub mod room_controller;
