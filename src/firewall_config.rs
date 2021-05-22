1fcaad4bff809a03b37833dbe1fdfa0ee4fb3f15
Benjamin Ludewig <ludewig.b@gmail.com>
2021-05-22 16:54:10 +0200

    Add LICENSE information

--- src/firewall_config.rs
+++ src/firewall_config.rs
@@ -1,3 +1,6 @@
+// Copyright 2020-2021, Benjamin Ludewig, Florian Bonetti, Jeffrey Munstermann, Luca Nittscher, Hugo Damer, Michael Bach
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
