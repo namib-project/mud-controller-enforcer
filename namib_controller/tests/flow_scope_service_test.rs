19286b7078be6f8e218dea5738a92924cd0afe31
Hannes Masuch <hama@uni-bremen.de>
2022-04-28 12:19:56 +0200

    feat(controller): Extend flow scope service tests

--- namib_controller/tests/flow_scope_service_test.rs
+++ namib_controller/tests/flow_scope_service_test.rs
@@ -1,3 +1,6 @@
+// Copyright 2022, Jasper Wiegratz, Hannes Masuch
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use chrono::{Duration, NaiveDateTime, Utc};
 use namib_controller::error::Result;
 use namib_controller::models::{Device, DeviceWithRefs};
@@ -13,9 +16,9 @@ async fn test_get_flow_scopes() -> Result<()> {
 
     let device_mac = SerdeMacAddr::V6(MacAddr6::new(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB));
     let device = DeviceWithRefs {
-        controller_uris: vec![],
+        controller_mappings: vec![],
         inner: Device {
-            id: 0,
+            id: 1,
             name: Some(String::from("test_device")),
             ipv4_addr: None,
             ipv6_addr: None,
@@ -27,11 +30,12 @@ async fn test_get_flow_scopes() -> Result<()> {
             collect_info: false,
             last_interaction: NaiveDateTime::from_timestamp(42_000_000, 0),
             room_id: None,
-            clipart: None,
+            fa_icon: None,
             q_bit: false,
         },
         mud_data: None,
         room: None,
+        quarantine_exceptions: vec![],
     };
     device_service::insert_device(&device, &ctx.db_conn).await?;
 
@@ -55,15 +59,16 @@ async fn test_get_flow_scopes() -> Result<()> {
             Utc::now().naive_utc().checked_sub_signed(Duration::seconds(1)).unwrap(),
         ),
     ];
+
     for scope_values in &scope_data {
         let device_macs = vec![
             device_mac,
             SerdeMacAddr::V6(MacAddr6::new(0x11, 0x22, 0x44, 0x66, 0x88, 0xAA)),
         ];
         flow_scope_service::insert_flow_scope(
+            device_macs,
             &FlowScope {
                 name: String::from(scope_values.0),
-                targets: Some(device_macs),
                 level: scope_values.1.clone(),
                 ttl: scope_values.2,
                 starts_at: scope_values.3,
@@ -74,8 +79,33 @@ async fn test_get_flow_scopes() -> Result<()> {
     }
 
     assert_eq!(flow_scope_service::get_active_flow_scopes(&ctx.db_conn).await?.len(), 1);
-    let scopes = flow_scope_service::get_all_flow_scopes(&ctx.db_conn).await?;
-    assert_eq!(scopes.len(), 3);
+    assert_eq!(flow_scope_service::get_all_flow_scopes(&ctx.db_conn).await?.len(), 3);
+
+    let id_empty = flow_scope_service::insert_flow_scope(
+        vec![],
+        &FlowScope {
+            name: "active_and_empty".to_string(),
+            level: Level::Full,
+            ttl: 7200,
+            starts_at: Utc::now().naive_utc().checked_sub_signed(Duration::seconds(1)).unwrap(),
+        },
+        &ctx.db_conn,
+    )
+    .await?;
+
+    assert_eq!(
+        flow_scope_service::get_active_flow_scopes_for_device(&ctx.db_conn, 1)
+            .await?
+            .len(),
+        1
+    );
+    assert_eq!(flow_scope_service::get_active_flow_scopes(&ctx.db_conn).await?.len(), 2);
+    let scopes: Vec<FlowScope> = flow_scope_service::get_all_flow_scopes(&ctx.db_conn)
+        .await?
+        .into_iter()
+        .map(|f| FlowScope::from(f))
+        .collect();
+    assert_eq!(scopes.len(), 4);
 
     scopes
         .iter()
@@ -83,11 +113,25 @@ async fn test_get_flow_scopes() -> Result<()> {
         .map(|(dbo, data)| {
             println!("Scope: {} {:?} {}", dbo.name, dbo.level, dbo.ttl);
             assert_eq!(dbo.name, data.0);
-            //assert_eq!(dbo.level, data.1);
+            assert_eq!(dbo.level, data.1);
             assert_eq!(dbo.ttl, data.2);
             println!("Scope: {} {:?} {}", data.0, data.1, data.2);
         })
         .count();
+
+    flow_scope_service::remove_flow_scope(id_empty, &ctx.db_conn).await?;
+    assert_eq!(flow_scope_service::get_all_flow_scopes(&ctx.db_conn).await?.len(), 3);
+
+    assert_eq!(
+        flow_scope_service::remove_targets_from_scope(vec![device_mac], 3, &ctx.db_conn).await?,
+        1
+    );
+    assert_eq!(
+        flow_scope_service::get_active_flow_scopes_for_device(&ctx.db_conn, 1)
+            .await?
+            .len(),
+        0
+    );
 
     Ok(())
 }
@@ -97,9 +141,9 @@ async fn test_get_flow_scope_by_name() -> Result<()> {
     let ctx = lib::IntegrationTestContext::new("test_get_flow_scope_by_name").await;
 
     flow_scope_service::insert_flow_scope(
+        Vec::new(),
         &FlowScope {
             name: String::from("test_scope"),
-            targets: Some(Vec::new()),
             level: Level::Full,
             ttl: 3600,
             starts_at: NaiveDateTime::from_timestamp(42_000_000, 0),
