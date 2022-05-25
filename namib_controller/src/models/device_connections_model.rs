d38c902bb8996c32df791e2675b5da52765b9071
schnitti <schnitti@uni-bremen.de>
2022-05-25 14:34:55 +0200

    feat(controller): add device connection dbo

---
+++ namib_controller/src/models/device_connections_model.rs
@@ -0,0 +1,14 @@
+use std::net::IpAddr;
+
+#[derive(Debug, Clone)]
+pub struct DeviceConnections {
+    pub device_id: i64,
+    pub from: Vec<DeviceConnection>,
+    pub to: Vec<DeviceConnection>,
+}
+
+#[derive(Debug, Clone)]
+pub struct DeviceConnection {
+    pub target: IpAddr,
+    pub amount: i64,
+}
