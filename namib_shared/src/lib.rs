631a901ae3e005c7dcd52f467736f68686816044
Hannes Masuch <hama@uni-bremen.de>
2022-06-01 11:21:27 +0200

    feat(shared): Rework flow scope ttl validation

--- namib_shared/src/lib.rs
+++ namib_shared/src/lib.rs
@@ -4,6 +4,7 @@
 #![warn(clippy::all, clippy::style, clippy::pedantic)]
 #![allow(clippy::must_use_candidate, clippy::default_trait_access)]
 
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use tokio_serde::formats::Cbor;
 
@@ -28,15 +29,22 @@ pub struct EnforcerConfig {
     version: String,
     devices: Vec<FirewallDevice>,
     secure_name: String,
+    expiration_date_time: Option<NaiveDateTime>,
 }
 
 impl EnforcerConfig {
     /// Construct a new firewall config with the given version and firewall rules
-    pub fn new(version: String, devices: Vec<FirewallDevice>, secure_name: String) -> Self {
+    pub fn new(
+        version: String,
+        devices: Vec<FirewallDevice>,
+        secure_name: String,
+        expiration_date_time: Option<NaiveDateTime>,
+    ) -> Self {
         EnforcerConfig {
             version,
             devices,
             secure_name,
+            expiration_date_time,
         }
     }
 
@@ -53,5 +61,10 @@ impl EnforcerConfig {
     /// Returns the secure dns name of the enforcer's controller
     pub fn secure_name(&self) -> &str {
         &self.secure_name
+    }
+
+    /// Returns the expiration date time of this config, based on the ttl of the active flow scopes
+    pub fn expiration_date_time(&self) -> Option<NaiveDateTime> {
+        self.expiration_date_time
     }
 }
--- namib_shared/src/rpc.rs
+++ namib_shared/src/rpc.rs
@@ -4,6 +4,7 @@
 #![allow(clippy::large_enum_variant)]
 
 use crate::{flow_scope::FlowData, models::DhcpEvent, EnforcerConfig};
+use chrono::NaiveDateTime;
 
 /// This is the interface for communication between enforcer & controller
 ///
@@ -11,7 +12,7 @@ use crate::{flow_scope::FlowData, models::DhcpEvent, EnforcerConfig};
 /// however function calls can only be made by the enforcer.
 #[tarpc::service]
 pub trait NamibRpc {
-    async fn heartbeat(version: Option<String>) -> Option<EnforcerConfig>;
+    async fn heartbeat(version: Option<String>, next_expiration: Option<NaiveDateTime>) -> Option<EnforcerConfig>;
     async fn dhcp_request(event: DhcpEvent);
     async fn send_logs(logs: Vec<String>);
     async fn send_scope_results(results: Vec<FlowData>);
